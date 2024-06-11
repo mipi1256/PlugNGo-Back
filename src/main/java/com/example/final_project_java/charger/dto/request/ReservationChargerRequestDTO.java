670508eb6301753dff4bdf26209ea31f04b34386
sseung <duaqkr2@naver.com>
2024-06-11 16:31:16 +0900

    ReservationCharger Entity / DTO / Repository / Controller / Service 파일만 작성

---
+++ src/main/java/com/example/final_project_java/charger/dto/request/ReservationChargerRequestDTO.java
@@ -0,0 +1,11 @@
+package com.example.final_project_java.charger.dto.request;
+
+import lombok.*;
+
+@Setter @Getter @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReservationChargerRequestDTO {
+}
---
+++ src/main/java/com/example/final_project_java/charger/dto/response/ReservationChargerResponseDTO.java
@@ -0,0 +1,11 @@
+package com.example.final_project_java.charger.dto.response;
+
+import lombok.*;
+
+@Setter @Getter @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReservationChargerResponseDTO {
+}
