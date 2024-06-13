ba805a475f47f54ca9dfcbfbca2ffbf77f0515bf
sseung <duaqkr2@naver.com>
2024-06-13 15:55:07 +0900

    Charge Entity 수정

--- src/main/java/com/example/final_project_java/charger/dto/response/ReservationChargerResponseDTO.java
+++ src/main/java/com/example/final_project_java/charger/dto/response/ReservationChargerResponseDTO.java
@@ -8,4 +8,13 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class ReservationChargerResponseDTO {
+
+    // 예약 확인 -> 이름, 휴대폰번호, 예약 시간, 가격, 충전소명, 충전소 위치
+    private String userName;
+    private String phoneNumber;
+    private int rentTime;
+    private int rentChargePrice;
+    private String stationName;
+    private String address;
+
 }
--- src/main/java/com/example/final_project_java/userapi/service/UserService.java
+++ src/main/java/com/example/final_project_java/userapi/service/UserService.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
    private final UserRepository userRepository;
-   private final PasswordEncoder passwordEncoder;
+//   private final PasswordEncoder passwordEncoder;
 
 }
 
