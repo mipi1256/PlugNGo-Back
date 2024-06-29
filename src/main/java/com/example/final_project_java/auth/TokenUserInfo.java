97f2f01464bbdbbe3545505266cc5693fd5af1f0
sseung <duaqkr2@naver.com>
2024-06-29 16:37:19 +0900

    로그인 시 회원정보 가져오기

--- src/main/java/com/example/final_project_java/auth/TokenUserInfo.java
+++ src/main/java/com/example/final_project_java/auth/TokenUserInfo.java
@@ -4,6 +4,8 @@ import com.example.final_project_java.userapi.entity.LoginMethod;
 import com.example.final_project_java.userapi.entity.Role;
 import lombok.*;
 
+import java.time.LocalDate;
+
 
 @Getter @ToString
 @EqualsAndHashCode
@@ -14,9 +16,9 @@ public class TokenUserInfo {
    private String userId;
    private String email;
    private String phoneNumber;
+   private LocalDate birthDay;
    private Role role;
    private LoginMethod loginMethod;
-   private String phoneNumber;
 }
 
 
