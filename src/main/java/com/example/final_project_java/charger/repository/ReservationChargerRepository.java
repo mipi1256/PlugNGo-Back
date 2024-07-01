cda96e8e5bb39918ef9210ddae9f7b01c24eccdb
SJHlll <ssaadd1212@gmail.com>
2024-07-01 14:57:17 +0900

    백엔드 중복 예약 x

--- src/main/java/com/example/final_project_java/charger/repository/ReservationChargerRepository.java
+++ src/main/java/com/example/final_project_java/charger/repository/ReservationChargerRepository.java
@@ -1,8 +1,10 @@
 package com.example.final_project_java.charger.repository;
 
 import com.example.final_project_java.charger.Entity.ReservationCharger;
+import com.example.final_project_java.userapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ReservationChargerRepository extends JpaRepository<ReservationCharger, String> {
+    boolean existsByUser(User user);
 }
