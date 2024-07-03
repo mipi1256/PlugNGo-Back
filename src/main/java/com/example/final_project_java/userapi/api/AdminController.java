2f2bac763681d0cd4209088079c8d27d77355114
SJHlll <ssaadd1212@gmail.com>
2024-07-03 17:11:25 +0900

    관리자 리뷰 컨트롤러 추가

--- src/main/java/com/example/final_project_java/userapi/api/AdminController.java
+++ src/main/java/com/example/final_project_java/userapi/api/AdminController.java
@@ -1,6 +1,8 @@
 package com.example.final_project_java.userapi.api;
 
 import com.example.final_project_java.charger.dto.response.ReservationChargerResponseDTO;
+import com.example.final_project_java.review.dto.response.ReviewDetailResponseDTO;
+import com.example.final_project_java.review.service.ReviewService;
 import com.example.final_project_java.userapi.service.MyPageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +21,21 @@ public class AdminController {
 
     private final MyPageService myPageService;
 
-    @GetMapping
-    public ResponseEntity<?> myReserveStation() {
-        log.info("/admin : GET!");
+    private final ReviewService reviewService;
+
+    @GetMapping("/station")
+    public ResponseEntity<?> reservedStation() {
+        log.info("/admin/station : GET!");
 
         List<ReservationChargerResponseDTO> responseDTO = myPageService.myReservedStation();
+        return ResponseEntity.ok().body(responseDTO);
+    }
+
+    @GetMapping("/review")
+    public ResponseEntity<?> writtenReview() {
+        log.info("/admin/review : GET!");
+
+        List<ReviewDetailResponseDTO> responseDTO = reviewService.getList().getReviews();
         return ResponseEntity.ok().body(responseDTO);
     }
 }
